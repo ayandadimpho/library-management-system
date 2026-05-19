7d85eb17fc326bac70996038f13e4b6ab39e38ba
Ayanda Mtolo <aymtojhb025@student.wethinkcode.co.za>
2026-05-19 12:50:05 +0200

    library system

--- practical/starter-code/src/main/java/library/LibraryItem.java
+++ practical/starter-code/src/main/java/library/LibraryItem.java
@@ -7,17 +7,62 @@ package library;
  *       and the behaviour specification in practical_instructions.md.
  */
 public class LibraryItem {
-
-    // TODO: Declare private fields:
-    //   - title        (String)
-    //   - isbn         (String)
-    //   - year         (int)
-    //   - isCheckedOut (boolean)
+    private String title;
+    private  String isbn;
+    private int year;
+    private boolean isCheckedOut;
 
 
-    // TODO: Constructor — LibraryItem(String title, String isbn, int year)
-    //   isCheckedOut must default to false.
 
+
+    private LibraryItem(String title, String isbn, int year) {
+        this.title = title;
+        this.isbn = isbn;
+        this.year = year;
+        this.isCheckedOut = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void checkOut(boolean isCheckedOut) {
+        if (isCheckedOut == true) {
+            throw new IllegalStateException("Item is already checked out.");
+        }
+        this.isCheckedOut = true;
+    }
+
+    public void returnItem(boolean isCheckedOut) {
+        if (isCheckedOut == false) {
+            throw new IllegalStateException("Item is not currently checked out.");
+        }
+        this.isCheckedOut = false;
+    }
+
+    public void setYear(int year) {
+        if (year < 1440 || year > year) {
+            throw new IllegalArgumentException;
+        }
+        this.year = year;
+    }
+
+    public String getSummary() {
+        Stringbuilder s = new Stringbuilder();
+        s.append
+    }
 
     // TODO: Getters — getTitle(), getIsbn(), getYear(), isCheckedOut()
 
